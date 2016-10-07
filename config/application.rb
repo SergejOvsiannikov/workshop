44ffe183f8e51a5b9c798fe6da81f97912647b7a
Sergej Ovsiannikov <sergey.ovsiannikov@jetruby.com>
2016-10-07 15:46:00 +0300

    Fix views

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Workshop1
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.enabled = true
+    config.assets.version = '1.0'
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
